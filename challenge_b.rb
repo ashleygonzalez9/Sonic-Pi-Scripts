da39fbbe58ed71baa7ded1529c45085fdf483683
ashleygonzalez9 <37843561+ashleygonzalez9@users.noreply.github.com>
2020-01-17 15:26:38 -0800

    Add files via upload

---
+++ challenge_b.rb
@@ -0,0 +1,28 @@
+=begin
+YOUR TASK: Using the most efficient code possible (loops and live loops),
+recreate the drum beat from the .wav file in this folder.
+
+SAMPLES USED: :drum_cymbal_closed, :drum_heavy_kick, :drum_snare_soft, :guit_e_slide
+
+=end
+
+#default tempo where one beat is one second
+use_bpm 60
+
+#cymbal only
+
+
+
+#cymbal and kick
+
+
+
+#cymbal and kick and snare
+
+
+
+#all live loops concurrently + guitar
+
+
+
+sample :drum_splash_hard
